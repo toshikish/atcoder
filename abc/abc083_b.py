744afcd36f02b0b01015b972ef85ff84567afffc
toshikish <toshiki@nanshika.com>
2019-03-31 20:17:19 +0900

    Modify ABC083_B

--- abc/abc083_b.py
+++ abc/abc083_b.py
@@ -1,12 +1,12 @@
 N, A, B = map(int, input().split())
 
-S = 0
+ans = 0
 for i in range(1, N + 1):
     original_i = i
     s = 0
     while i != 0:
         s += i % 10
-        i = int(i / 10)
-    if s >= A and s <= B:
-        S += original_i
-print(S)
+        i //= 10
+    if A <= s <= B:
+        ans += original_i
+print(ans)
