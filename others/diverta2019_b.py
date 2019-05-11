4d4ed7a073b6743770d39a66976b668796e81485
toshikish <toshiki@nanshika.com>
2019-05-11 21:29:58 +0900

    Add diverta2019_B

---
+++ others/diverta2019_b.py
@@ -0,0 +1,8 @@
+R, G, B, N = map(int, input().split())
+
+ans = 0
+for r in range(N // R + 1):
+    for g in range((N - R * r) // G + 1):
+        if (N - R * r - G * g) % B == 0:
+            ans += 1
+print(ans)
