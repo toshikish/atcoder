5503331e72e78baafb51ea6339aff12e12af4e31
toshikish <toshiki@nanshika.com>
2019-05-25 21:32:21 +0900

    Add ABC127_D

---
+++ abc/abc127_d.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+d = defaultdict(int)
+for i in range(N):
+    d[A[i]] += 1
+for i in range(M):
+    Bi, Ci = map(int, input().split())
+    d[Ci] += Bi
+
+res = N
+ans = 0
+print(sorted(d.items(), reverse=True))
+for k, v in sorted(d.items(), reverse=True):
+    ans += k * min(v, res)
+    res -= v
+    if res <= 0:
+        break
+print(ans)
