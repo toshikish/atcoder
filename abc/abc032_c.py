83ffd61d83d521cca5cd58d47a48c76afa36ad9f
toshikish <toshiki@nanshika.com>
2019-07-10 23:53:29 +0900

    Add ABC032_C

---
+++ abc/abc032_c.py
@@ -0,0 +1,17 @@
+N, K = map(int, input().split())
+s = [int(input()) for i in range(N)]
+
+if 0 in s:
+    ans = N
+else:
+    ans = 0
+    r = 0
+    p = 1
+    for l in range(N):
+        while r < N and p * s[r] <= K:
+            p *= s[r]
+            r += 1
+        ans = max(ans, r - l)
+        if l < r:
+            p //= s[l]
+print(ans)
