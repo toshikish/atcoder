9f8a56ae69b345efbf6f15d8811fbe638597c4b0
Toshiki Shimomura <toshiki@nanshika.com>
2019-07-11 13:22:09 +0900

    Add ABC130_D

---
+++ abc/abc130_d.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+a = list(map(int, input().split()))
+
+ans = 0
+s = 0
+r = 0
+for l in range(N):
+    while s < K and r < N:
+        s += a[r]
+        r += 1
+    if s < K:
+        break
+    ans += N - r + 1
+    s -= a[l]
+print(ans)
