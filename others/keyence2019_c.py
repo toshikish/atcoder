f83edbd192187cab4baa57044f7554d870103afb
toshikish <toshiki@nanshika.com>
2019-08-17 18:32:33 +0900

    Fix Keyence2019_C

--- others/keyence2019_c.py
+++ others/keyence2019_c.py
@@ -1,32 +1,23 @@
-import pprint
-from collections import defaultdict
-import sys
-n = int(input())
-a = list(map(int, input().rstrip().split()))
-b = list(map(int, input().rstrip().split()))
+import heapq
+
+N = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
 if sum(a) < sum(b):
-    print(-1)
-    sys.exit()
-min_i = 0
-residue = 0
-repos = defaultdict(int)
-for i in range(n):
-    tmp = a[i] - b[i]
-    if tmp < 0:
-        min_i += 1
-        residue = -tmp
-    elif tmp > 0:
-        repos[tmp] += 1
-repo_keys = sorted(repos.keys(), reverse=True)
-#pprint.pprint(repo_keys)
-print(min_i)
-while residue > 0:
-    k = repo_keys.pop(0)
-    v = repos[k]
-    if k * v >= residue:
-        min_i += int(residue / k) + (residue % k > 0)
-        break
-    else:
-        residue -= k * v
-        min_i += v
-print(min_i)
+    ans = -1
+else:
+    ans = 0
+    res = 0
+    rep = []
+    for i in range(N):
+        diff = b[i] - a[i]
+        if diff > 0:
+            ans += 1
+            res += diff
+        elif diff < 0:
+            heapq.heappush(rep, diff)
+    while res > 0:
+        ans += 1
+        res += heapq.heappop(rep)
+print(ans)
