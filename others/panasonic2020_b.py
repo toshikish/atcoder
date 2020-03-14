9d93cd2b55a20c7cc79c9cfd19cd6b905ce976c3
toshikish <toshiki@nanshika.com>
2020-03-15 00:53:28 +0900

    Refactor Panasonic2020_B

--- others/panasonic2020_b.py
+++ others/panasonic2020_b.py
@@ -1,14 +1,2 @@
 H, W = map(int, input().split())
-
-ans = 0
-if H == 1 or W == 1:
-    ans = 1
-elif W % 2 == 0:
-    ans = W // 2 * H
-else:
-    e = (W + 1) // 2
-    o = W // 2
-    ans = (e + o) * (H // 2)
-    if H % 2 == 1:
-        ans += e
-print(ans)
+print(1 if H == 1 or W == 1 else (H * W + 1) // 2)
