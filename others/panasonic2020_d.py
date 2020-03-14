dbfe5e24f9c0b1747a9c16b41ded0ee082ab284b
toshikish <toshiki@nanshika.com>
2020-03-15 00:52:43 +0900

    Refactor Panasonic2020_D

--- others/panasonic2020_d.py
+++ others/panasonic2020_d.py
@@ -1,16 +1,13 @@
 N = int(input())
 
 
-def l(i):
-    return chr(i + 96)
-
-
-def f(i, L):
-    if i == N + 1:
-        print(''.join(list(map(l, L))))
+def f(L):
+    l = len(L)
+    if l == N:
+        print(''.join(list(map(lambda i: chr(i + 96), L))))
         return
-    for d in range(1, min(i + 1, len(set(L)) + 2)):
-        f(i + 1, L + [d])
+    for d in range(1, min(l, max(L)) + 2):
+        f(L + [d])
 
 
-f(1, [])
+f([1])
