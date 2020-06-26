6d92ececd9a0a7733af86873cd4946ea198286c2
toshikish <toshiki@nanshika.com>
2020-06-26 23:40:51 +0900

    Add ABC099_D

---
+++ abc/abc099_d.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from itertools import permutations
+
+N, C = map(int, input().split())
+D = [list(map(int, input().split())) for i in range(C)]
+c = [list(map(lambda s: int(s) - 1, input().split())) for i in range(N)]
+
+d = [Counter(sum([c[j][(i - j) % 3::3] for j in range(N)], []))
+     for i in range(3)]
+ans = 1000000007
+for t in permutations(range(C), 3):
+    dis = 0
+    for i in range(3):
+        for k, v in d[i].items():
+            dis += D[k][t[i]] * v
+    ans = min(dis, ans)
+print(ans)
