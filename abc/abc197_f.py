1469e1dfa0192c9a991dfda34e9fd66a5f56db23
toshikish <toshiki@nanshika.com>
2021-04-04 01:42:44 +0900

    Refactor ABC197_F

--- abc/abc197_f.py
+++ abc/abc197_f.py
@@ -2,16 +2,20 @@ from collections import deque
 
 N, M = map(int, input().split())
 g = [[] for _ in range(N)]
+adj = [[False] * N for _ in range(N)]
 for _ in range(M):
     Ai, Bi, Ci = input().split()
-    Ai, Bi = map(lambda s: int(s) - 1, [Ai, Bi])
+    Ai = int(Ai) - 1
+    Bi = int(Bi) - 1
     Ci = ord(Ci) - ord('a')
     g[Ai].append((Bi, Ci))
     g[Bi].append((Ai, Ci))
+    adj[Ai][Bi] = adj[Bi][Ai] = True
 
 INF = 1000000007
 dp = [[INF] * N for _ in range(N)]
 q = deque()
+ans = INF
 
 
 def push(a, b, c):
@@ -24,12 +28,18 @@ def push(a, b, c):
 push(0, N - 1, 0)
 while q:
     a, b, c = q.popleft()
+    if c >= ans:
+        break
+    if a == b:
+        ans = c
+        break
+    if adj[a][b]:
+        ans = c + 1
+        continue
     for ea, ca in g[a]:
         for eb, cb in g[b]:
             if ca != cb:
                 continue
-            push(ea, eb, dp[a][b] + 1)
+            push(ea, eb, dp[a][b] + 2)
 
-ans = min([INF] + [dp[i][i] * 2 for i in range(N)]
-          + [dp[i][ei] * 2 + 1 for i in range(N) for ei, _ in g[i]])
 print(-1 if ans == INF else ans)
