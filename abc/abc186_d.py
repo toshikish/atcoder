467c3ca67164762c71baabb76850220cd326060a
toshikish <toshiki@nanshika.com>
2021-07-06 01:18:39 +0900

    Modify ABC186_D

--- abc/abc186_d.py
+++ abc/abc186_d.py
@@ -5,4 +5,5 @@ A = list(map(int, input().split()))
 
 A.sort()
 S = list(accumulate(A))
-print(sum(S[N - 1] - S[i] - (N - i - 1) * A[i] for i in range(N - 1)))
+# print(sum(S[N - 1] - S[i] - (N - i - 1) * A[i] for i in range(N - 1)))
+print(sum(i * A[i] - S[i - 1] for i in range(1, N)))
